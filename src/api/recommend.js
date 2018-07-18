e4d0319c5a522dd6ce04d3696c18d58a6405dd87
silence <slience4823@163.com>
2018-07-18 23:14:02 +0800

    1.0

---
+++ src/api/recommend.js
@@ -0,0 +1,36 @@
+import jsonp from 'common/js/jsonp'
+import {
+  commonParam,
+  options
+} from 'api/config'
+import axios from 'axios'
+
+export function getRecommend () {
+  const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
+
+  const data = Object.assign({}, commonParam, {
+    platform: 'h5',
+    uin: 0,
+    needNewCode: 1
+  })
+  return jsonp(url, data, options)
+}
+
+export function getDiscList () {
+  const url = '/api/getDiscList'
+  const data = Object.assign({}, commonParam, {
+    platform: 'yqq',
+    hostUin: 0,
+    sin: 0,
+    ein: 29,
+    sortId: 5,
+    categoryId: 10000000,
+    needNewCode: 0,
+    rnd: Math.random()
+  })
+  return axios.get(url, {
+    params: data
+  }).then((res) => {
+    return Promise.resolve(res.data)
+  })
+}

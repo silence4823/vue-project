009a5c3f08a064d80a74d3216168034ca7eb3fe2
silence <slience4823@163.com>
2018-07-25 23:33:19 +0800

    迭代

--- src/base/song-list/song-list.vue
+++ src/base/song-list/song-list.vue
@@ -1,21 +1,35 @@
 <template>
   <div class="song-list">
-    <ul></ul>
+    <ul>
+      <li v-for="song in songs" :key="song.id" class="item">
+        <div class="content">
+          <h2 class="name">{{song.name}}</h2>
+          <p class="desc">{{getDesc(song)}}</p>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
 export default {
-    props: {
-        
+  props: {
+    songs: {
+      type: Array,
+      default: null
     }
+  },
+  methods: {
+    getDesc (song) {
+      return `${song.singer} ● ${song.album}`
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped rel="stylesheet/stylus">
 @import "~common/stylus/variable"
 @import "~common/stylus/mixin"
-
 .song-list
   .item
     display: flex
@@ -24,14 +38,14 @@ export default {
     height: 64px
     font-size: $font-size-medium
     .content
-    flex: 1
-    line-height: 20px
-    overflow: hidden
-    .name
-      no-wrap()
-      color: $color-text
-    .desc
-      no-wrap()
-      margin-top: 4px
-      color: $color-text-d
+      flex: 1
+      line-height: 20px
+      overflow: hidden
+      .name
+        no-wrap()
+        color: $color-text
+      .desc
+        no-wrap()
+        margin-top: 4px
+        color: $color-text-d
 </style>
--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -4,13 +4,21 @@
         <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
-    <div class="bg-image" :style="bgStyle">
+    <div class="bg-image" :style="bgStyle" ref="bgImage">
       <div class="filter"></div>
     </div>
+    <scroll :data="songs" class="list" ref="list">
+      <div class="song-list-wrapper">
+        <song-list :songs="songs"></song-list>
+      </div>
+    </scroll>
  </div>
 </template>
 
 <script type="text/ecmascript-6">
+import Scroll from 'base/scroll/scroll'
+import SongList from 'base/song-list/song-list'
+
 export default {
   props: {
     bgImage: {
@@ -26,10 +34,17 @@ export default {
       default: ''
     }
   },
+  mounted () {
+    this.$refs.list.$el.style.top = `${this.$refs.bgImage.clientHeight}px`
+  },
   computed: {
     bgStyle () {
       return `background-image:url(${this.bgImage})`
     }
+  },
+  components: {
+    Scroll,
+    SongList
   }
 }
 </script>
@@ -99,10 +114,16 @@ export default {
           vertical-align: middle
           font-size: $font-size-small
     .filter
-        position: absolute
-        top: 0
-        left: 0
-        width: 100%
-        height: 100%
-        background: rgba(7, 17, 27, 0.4)
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      background: rgba(7, 17, 27, 0.4)
+  .list
+    position: fixed
+    top: 0
+    bottom: 0
+    width: 100%
+    background: $color-background
 </style>
